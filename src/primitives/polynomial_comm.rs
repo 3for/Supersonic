729862056cfb17eb8795dde963cd4bad1bfc04d1
root <287494524@qq.com>
2021-02-08 17:24:46 +0800

    pp

--- src/primitives/polynomial_comm.rs
+++ src/primitives/polynomial_comm.rs
@@ -1,6 +1,9 @@
 use crate::groups::UnknownOrderGroup;
+//use rand::CryptoRng;
+//use rand::Rng;
 
 /// [Follow the idea in `https://github.com/ZenGo-X/class/src/primitives`]
+/// AND [Follow the idea in `https://github.com/dignifiedquire/rust-accumulator/src/accumulator`]
 /// Polynomial commitment as given in the paper: Transparent SNARKs from DARK Compilers
 /// (https://eprint.iacr.org/2019/1229.pdf), subsection 4.2 and 4.3
 /// The following algorithms are implemented:
@@ -14,8 +17,26 @@ use crate::groups::UnknownOrderGroup;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct PP<G: UnknownOrderGroup> {
+    pub security_level: usize,
     pub disc: G::Elem,
     pub g: G::Elem,
     pub q: G::Elem,
     pub p: G::Elem,
 }
+
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct PolyComm {
+    pub c: G::Elem,
+}
+
+impl PolyComm {
+    /*pub fn KeyGen_RSAsetup<G, R>(rng: &mut R, _lambda: usize) -> PP 
+    where 
+        G: UnknownOrderGroup,
+        R: CryptoRng + Rng,
+    {
+
+    }*/
+}
+
